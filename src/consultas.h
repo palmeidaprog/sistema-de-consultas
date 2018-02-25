da3ddaf3d32c1e7b5b9ae69b0d3282278344c18e
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2018-02-24 22:34:30 -0300

    [Sync] teste

--- src/consultas.h
+++ src/consultas.h
@@ -6,3 +6,12 @@
  * Aluno: Paulo R. Almeida Filho
  * E-mail: pauloalmeidaf@gmail.com
  */
+
+#ifndef CONSULTAS_H
+#define CONSULTAS_H
+
+typedef struct Consulta {
+    int n;
+} Consultas;
+
+#endif
