ebfe6f7e7b20cac0639204ff0d73bcf7a84c928b
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2018-03-26 15:26:46 -0300

    Modificado sistema de entrada de dados de fgets para getche. Implementados uma versao especial para linux/mac os com termios.h

--- src/io.c
+++ src/io.c
@@ -18,7 +18,47 @@ void limpaTela() {
     #endif
 }
 
+#if !defined(Win32) && !defined(_Win32) // nao é windows
+int getche() {
+      int c = 0;
+
+      struct termios org_opts, new_opts;
+      int res = 0;
+          //-----  store old settings -----------
+      res = tcgetattr(STDIN_FILENO, &org_opts);
+      assert(res == 0);
+          //---- set new terminal parms --------
+      memcpy(&new_opts, &org_opts, sizeof(new_opts));
+      new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT 
+        | ECHOKE | ICRNL);
+      tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+      c = getchar();
+          //------  restore old settings ---------
+      res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+      assert(res == 0);
+      printf("%c", c);
+      return(c);
+}
+#endif
+
 void pegaString(char *str, size_t n) {
+    int i = 0;
+    n -= 2;
+    
+    while(i < n) {
+        str[i] = getche();
+        if(str[i] == '\n') {
+            break;
+        }
+        ++i;
+    }
+    str[i] = '\0';
+    if(i == n) {
+        printf("\n");
+    }
+}
+
+/*void pegaString(char *str, size_t n) {
     size_t len;
 
     do {
@@ -28,7 +68,8 @@ void pegaString(char *str, size_t n) {
     if(str[len] == '\n') {
         str[len] = '\0';
     }
-}
+}*/
+
 
 void limpaBuffer() {
     char c;
--- src/io.h
+++ src/io.h
@@ -13,6 +13,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#if !defined(Win32) && !defined(_Win32) // windows
+#include <termios.h>
+#include <unistd.h>
+#include <assert.h>
+#endif
+
 
 void limpaBuffer();
 void limpaTela();
